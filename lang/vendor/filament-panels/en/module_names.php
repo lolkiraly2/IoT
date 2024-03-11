d383d2472705e099ec133a94b5bcd4af72eeb3be
lolkiraly2 <szebigeri@gmail.com>
2024-03-11 18:39:59 +0100

    2. fejezet

---
+++ lang/vendor/filament-panels/en/module_names.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'navigation_groups' => [
+    'administration' => 'Administration',
+  ],
+  'device_types' => [
+    'label' => 'Device type',
+    'plural_label' => 'Device types',
+  ],
+];
